81773b984334e4a17bb095870b5c5a140eabebd7
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-06-27 17:23:56 +0500

    initial commit

---
+++ M7/Classroom Activities 7/L1-A1.py
@@ -0,0 +1,8 @@
+# Printing a greeting
+print("Hello People!")
+# Printing my name
+print("My name is Tazkeer Dua Samak")
+# Printing a fun fact about myself
+print("I like to play games")
+# Performing a simple calculation
+print("The sum of 45 and 87 is :", 45 + 87)
---
+++ M7/Classroom Activities 7/L1-A4.py
@@ -0,0 +1,5 @@
+print("Welcome to the world of programming!")
+print(8)
+print("HI! \n I'm in the next line!")
+import keyword
+print(keyword.kwlist)
