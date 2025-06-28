bac49c27b89f2973adfc24466afcbbcbeee443e7
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-06-28 17:19:17 +0500

    initial commit

---
+++ M7/Classroom Activities 7/L2-A3.py
@@ -0,0 +1,17 @@
+first_name = "Tazkeer"
+middle_name = "Dua"
+last_name = "Samak"
+full_name = first_name+" "+middle_name+" "+last_name
+example = "Ha"*50
+
+print("First Name is : ", first_name)
+print("Middle Name is : ", middle_name)
+print("Last Name is : ", last_name)
+print("Full Name is : ", full_name)
+print("String multiplied by 50 is : ", example)
+
+word = 'Skating'
+print("Length of string : ", len(word))
+print("First Letter of string is : ", word[0])
+print("Last Letter of string is : ", word[6])
+print("String Sliced : ", word[0:4])
