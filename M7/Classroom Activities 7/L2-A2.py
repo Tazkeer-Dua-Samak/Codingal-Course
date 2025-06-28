d8e396a1c695d8246f28de7825f9474375b0a440
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-06-28 17:07:59 +0500

    initial commit

---
+++ M7/Classroom Activities 7/L2-A2.py
@@ -0,0 +1,21 @@
+num1 = 34
+num2 = 3
+
+print("Number 1 is : ", num1)
+print("Number 2 is : ", num2)
+print("Addition : ", num1+num2)
+print("Difference : ", num1-num2)
+print("Product : ", num1*num2)
+print("Division : ", num1/num2)
+print("Floor Division : ", num1//num2)
+print("Modulus : ", num1%num2)
+print("Square : ", num1**num2)
+print("Square Root : ", num1**0.5)
+
+print("Equal?", num1==num2)
+print("Is Number 1 greater?", num1>num2)
+print("Is Number 2 greater?", num1<num2)
+print("Not Equal?", num1!=num2)
+
+result = num1/2+num2**2-10
+print("result of the equation is : ", result)
