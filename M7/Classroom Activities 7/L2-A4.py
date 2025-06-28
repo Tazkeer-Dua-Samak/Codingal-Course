42d018d966d90ce3ad953835f974b3a945e4b97d
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-06-28 17:28:25 +0500

    initial commit

---
+++ M7/Classroom Activities 7/L2-A4.py
@@ -0,0 +1,12 @@
+x = input("Enter a value of x : ")
+y = input("Enter a value of y : ")
+
+temp = x
+x = y
+y = temp
+
+#Single Line Swap
+# x, y = y, x
+
+print("Value of x after swapping : ", x)
+print("Value of y after swapping : ", y)
