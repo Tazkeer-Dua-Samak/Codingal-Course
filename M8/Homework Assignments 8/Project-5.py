4205845fe04230bdfd92690450c665b96e8f3f52
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-08-08 10:25:22 +0500

    initial commit

--- M8/Homework Assignments 8/Project-5.py
+++ M8/Homework Assignments 8/Project-5.py
@@ -1,23 +1,30 @@
 import math
 
-class Polygon:
+class Shape:
+    def __init__(self, name):
+        self.name = name  
+
+    def display_name(self):
+        print(f"This is a {self.name}.")
+
+class Polygon(Shape):
     def __init__(self, sides, length):
-        self.sides = sides    
-        self.length = length  
+        super().__init__("Polygon")
+        self.sides = sides
+        self.length = length
 
     def calculate_area(self):
         area = (self.sides * (self.length ** 2)) / (4 * math.tan(math.pi / self.sides))
         return area
 
 def main():
-   
     sides = int(input("Enter the number of sides of the polygon: "))
     length = float(input("Enter the length of each side: "))
     
     polygon = Polygon(sides, length)
     
+    polygon.display_name() 
     area = polygon.calculate_area()
     print(f"The area of the polygon is: {area:.2f}")
-
+    
 obj = main()
-
