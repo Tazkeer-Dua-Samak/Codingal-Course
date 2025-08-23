76be0ecc8b5175d2d760fcfb70e43931671e14f8
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-08-23 16:31:15 +0500

    initial commit

--- M9/Homework Assignments 9/Project-4.py
+++ M9/Homework Assignments 9/Project-4.py
@@ -1,10 +1,19 @@
 import random
-import string
 
 def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_."
+    password = ""
+    for _ in range(length):
+        password += random.choice(characters)
     return password
 
-length = int(input("Enter the desired password length: "))
-print("Generated Password:", generate_password(length))
+# The following code is getting the length from the user while making sure the user does not enter anything other than the length of password.
+try:
+    length = int(input("Enter the desired password length: "))
+    if length <= 0:
+        print("Password length must be a positive number.")
+    else:
+        password = generate_password(length)
+        print("Generated Password:", password)
+except ValueError:
+    print("Invalid input. Please enter a number.")
