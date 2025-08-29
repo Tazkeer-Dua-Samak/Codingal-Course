4b84710d73a9d5caf9c37830080f1479240029c5
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-08-29 16:59:38 +0500

    initial commit

---
+++ M10/Homework Assignments 10/Project-1.py
@@ -0,0 +1,10 @@
+def oneIteration(n, m):
+    return n*m
+print(oneIteration(4, 5))
+
+def nIterations(n, m):
+    product=0
+    for i in range(n):
+        product+=1
+    return product
+print(nIterations(5, 4))
