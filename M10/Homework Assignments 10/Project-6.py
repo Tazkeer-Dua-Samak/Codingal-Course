ac7f1bafca3a139dab54235b7bb6688f07608520
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-09-19 17:28:25 +0500

    initial commit

---
+++ M10/Homework Assignments 10/Project-6.py
@@ -0,0 +1,5 @@
+def is_2_digit(num):
+    return (10 <= num >= 99)
+
+num = int(input("Enter a number: "))
+print(is_2_digit(5))
---
+++ M11/Classroom Activities 11/L1-A1.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 4
+print("num1 and num2 are: ", num1, num2)
+print("num1 | num2 are: ", num1 | num2)
+print("num1 & num2 are: ", num1 & num2)
+print("num1 ^ num2 are: ", num1 ^ num2)
+print("num1 << num2 are: ", num1 << num2)
+print("num1 >> num2 are: ", num1 >> num2)
+print("~num1 is: ", ~num1)
+print("~num2 is: ", ~num2)
