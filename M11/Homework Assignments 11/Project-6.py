9f8e8128544a21fd3903820b304630c52531afef
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-10-12 21:34:37 +0500

    initial commit

--- M11/Homework Assignments 11/Project-6.py
+++ M11/Homework Assignments 11/Project-6.py
@@ -1,13 +1,12 @@
-def printSubStrings(s, size):
-    powerSetSize = 1 << size  # same as 2^size but using bitwise
-
-    for outer in range(1, powerSetSize):  # start from 1 to skip empty substring
-        result = ""
-        for inner in range(size):
-            if (outer & (1 << inner)) > 0:
-                result += s[inner]
-        print(result)
-
+def printSubStrings(s):
+    size = len(s)
+    
+    for i in range(1, 1 << size):
+        substring = ""  
+        for j in range(size):
+            if (i & (1 << j)) > 0:
+                substring += s[j]
+        print(substring)
 
 string = input("Enter a string: ")
-printSubStrings(string, len(string))
+printSubStrings(string)
