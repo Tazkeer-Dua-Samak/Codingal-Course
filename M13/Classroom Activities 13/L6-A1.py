512c48a65e5ef6233975b05526821e944130ece7
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-12-12 17:20:57 +0500

    initial commit

---
+++ M13/Classroom Activities 13/L6-A1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sqlite3
+
+database = 'database.sqlite'
+conn = sqlite3.connect(database)
+
+tables = pd.read_sql("""SELECT *
+                     FROM sqlite_master
+                     WHERE type='table' ;""", conn)
+print(tables)
+
+match = pd.read_sql("""SELECT * FROM Match""", conn)
+print("Match", match)
+
+venue = pd.read_sql("""SELECT * FROM Venue""", conn)
+print("Venue", venue)
+
+city = pd.read_sql("""SELECT * FROM City""", conn)
+print("City", city)
+
+team = pd.read_sql("""SELECT * FROM Team""", conn)
+print("Team", team)
+
+match_details = pd.read_sql('''SELECT m.Season_Id, m.Match_Id,
+                            v.Venue_Name, c.City_Name, t.Team_Name AS Winner
+                            FROM Match AS m
+                            INNER JOIN Venue AS v ON m.Venue_Id = v.Venue_Id
+                            INNER JOIN City AS c ON v.City_Id = c.City_Id
+                            INNER JOIN Team AS t ON m.Match_Winner = t.Team_Id
+                            ''', conn)
+print(match_details)
+print(match.columns)
