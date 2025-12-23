ae794db949422a38fc11a59b08d53da420db95a6
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2025-12-23 08:50:41 +0500

    initial commit

--- M13/Homework Assignments 13/Project-5.py
+++ M13/Homework Assignments 13/Project-5.py
@@ -4,25 +4,34 @@ import sqlite3
 database = 'basketball.sqlite'
 conn = sqlite3.connect(database)
 
-tables = pd.read_sql("""SELECT *
+tables = pd.read_sql("""
+                     SELECT *
                      FROM sqlite_master
-                     WHERE type='table' ;""", conn)
-print("\nALL TABLES \n")
+                     WHERE type = 'table';
+                     """, conn)
 print(tables)
 
-players = pd.read_sql("""SELECT is_active 
-                      FROM Player""", conn)
-print("\nALL FROM PLAYERS\n")
-print(players)
+player = pd.read_sql("""
+                     SELECT *
+                     FROM Player;
+                     """, conn)
+print(player)
 
-player_salary = pd.read_sql("""SELECT value
-                      FROM Player_Salary""", conn)
-print("\nALL FROM PLAYERS SALARY\n")
+player_salary = pd.read_sql("""
+                            SELECT *
+                            FROM Player_Salary;
+                            """, conn)
 print(player_salary)
 
-joining_tables = pd.read_sql("""SELECT DISTINCT nameTeam, namePlayer, value
-                                          FROM Player_Salary 
-                                          AND (SELECT is_active 
-                                          FROM Player)
-                                         """, conn)
-print(joining_tables)
+joined_salary = pd.read_sql("""
+                            SELECT ps.nameTeam,
+                                   ps.namePlayer,
+                                   ps.value,
+                                   p.is_active
+                            FROM Player_Salary ps
+                            INNER JOIN Player p
+                            ON ps.namePlayer = p.full_name
+                            GROUP BY ps.nameTeam;
+                            """, conn)
+print(joined_salary)
+
