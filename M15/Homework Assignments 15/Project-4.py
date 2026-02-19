ef163d57fee6b59a4db743531e8b3a00aa9342cb
Tazkeer-Dua-Samak <duasamaktazkeer@gmail.com>
2026-02-19 16:44:57 +0500

    initial commit

---
+++ M15/Homework Assignments 15/Project-4.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+ 
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+new_births = [12, 15, 11, 9, 1, 9, 21]
+
+plt.plot(day, new_births)
+plt.title("Birth Rate of July")
+plt.xlabel('Day')
+plt.ylabel('Birth Rate')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+Modern AUs: Something Real (wrong number/texting AU) and An Apple a Day (medical AU) are top-tier choices for non-cultivation settings.
+Humour: save a sword, ride a socialist by sysrae is frequently cited for its hilarity and unique take on the setting.
+Time Travel/De-aging: An Arrow Through Time and various de-aging fics where Lan Wangji has to care for a child version of Wei Wuxian are fan staples. 
+Reddit
+Reddit
+ +4
+For more curated lists, you can check WangXian Fic Recs on Tumblr or the MDZS Subreddit. 
+Reddit
+Reddit
+ +1
+Would you like recommendations for a specific trope (like time travel or modern AU) or a particular mood (like heavy angst or pure fluff)?
+
+
+'''
